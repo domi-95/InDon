9c99da28692bd8029b8ec8a5f7d7437ce6e2c3ed
Kevin <Kevin@Kevin-PC.fritz.box>
2018-02-13 22:40:08 +0100

    FUck the system

---
+++ TestInDon/src/bean/copy/LoginBean.java
@@ -0,0 +1,26 @@
+package bean.copy;
+
+public class LoginBean {
+	private String benutzername,pass;  
+	
+	public LoginBean (String benutzername, String passwort) {
+		this.benutzername = benutzername;
+		this.pass = passwort;
+	}
+	  
+	public String getBenutzername() {  
+	    return benutzername ;  
+	}  
+	  
+	public void setBenutzername(String benutzername) {  
+	    this.benutzername = benutzername;  
+	}  
+	  
+	public String getPass() {  
+	    return pass;  
+	}  
+	  
+	public void setPass(String pass) {  
+	    this.pass = pass;  
+	}  
+}
---
+++ TestInDon/src/bean/copy/Provider.java
@@ -0,0 +1,9 @@
+package bean.copy;
+
+public interface Provider {
+	String DRIVER = ".commysql.jdbc.Driver";
+	String CONNECTION_URL = "jdbc:mysql://server2.febas.net/InDon";
+	String USERNAME = "InDonAdmin";
+	String PASSWORD = "InDon123";
+
+}
