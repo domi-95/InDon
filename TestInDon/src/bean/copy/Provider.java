e46a916b4a32a9dfede5758a5f2f7d050b8f9122
Domi <Domi@dominiks-mbp>
2018-02-13 22:46:04 +0100

    I Hate it

--- TestInDon/src/bean/copy/Provider.java
+++
@@ -1,9 +0,0 @@
-package bean.copy;
-
-public interface Provider {
-	String DRIVER = ".commysql.jdbc.Driver";
-	String CONNECTION_URL = "jdbc:mysql://server2.febas.net/InDon";
-	String USERNAME = "InDonAdmin";
-	String PASSWORD = "InDon123";
-
-}
