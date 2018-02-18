ac2427fc1a2fb969d113a2731dfaa328f51b07c8
Kevin <Kevin@Kevin-PC>
2018-02-18 09:30:25 +0100

    Verschwundene Klasse hinzugefügt

---
+++ TestInDon/src/benutzer/Benutzer.java
@@ -0,0 +1,21 @@
+package benutzer;
+
+public class Benutzer {
+	
+	private int id;
+	private String benutzername;
+	private String passwort;
+	
+	public Benutzer(int id, String benutzername, String passwort) {
+		this.id = id;
+		this.benutzername = benutzername;
+		this.passwort = passwort;
+	}
+	
+
+	
+	
+	
+	
+	
+}
