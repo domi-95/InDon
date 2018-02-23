b497697bfa068eb27d6e9848ee43d0fc2cd1f052
kehm2006 <kehm2006@DE4L04772.de.int.kaufland>
2018-02-23 22:06:13 +0200

    müll

--- TestInDon/src/datenbank/ConnectionProvider.java
+++ TestInDon/src/datenbank/ConnectionProvider.java
@@ -10,9 +10,10 @@ public class ConnectionProvider {
 		try {
 			Class.forName(DRIVER);
 		//	con = DriverManager.getConnection("jdbc:mysql://server2.febas.net/InDon","InDonAdmin","InDon123");
-			con = DriverManager.getConnection(CONNECTION_URL, USERNAME, PASSWORD);
+			con = DriverManager.getConnection("jdbc:mysql://db725808780.db.1and1.com/db725808780","dbo725808780","InDon123");
+			//con = DriverManager.getConnection(CONNECTION_URL, USERNAME, PASSWORD);
 		} catch (Exception e) {
-			System.out.println("Connection nicht möglich zum DB");
+			System.out.println("Connection nicht möglich zur DB");
 		}
 	}
 
