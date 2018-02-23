e67851e08e2cc6925f3ffb23d49f0de791102949
kehm2006 <kehm2006@DE4L04772.de.int.kaufland>
2018-02-23 22:10:26 +0200

    miüll

--- TestInDon/src/datenbank/ConnectionProvider.java
+++ TestInDon/src/datenbank/ConnectionProvider.java
@@ -9,19 +9,11 @@ public class ConnectionProvider {
 	static {
 		try {
 			Class.forName(DRIVER);
-<<<<<<< HEAD
 		//	con = DriverManager.getConnection("jdbc:mysql://server2.febas.net/InDon","InDonAdmin","InDon123");
-			con = DriverManager.getConnection("jdbc:mysql://db725808780.db.1and1.com/db725808780","dbo725808780","InDon123");
-			//con = DriverManager.getConnection(CONNECTION_URL, USERNAME, PASSWORD);
-=======
+		//	con = DriverManager.getConnection("jdbc:mysql://db725808780.db.1and1.com/db725808780","dbo725808780","InDon123");
 			con = DriverManager.getConnection("jdbc:mysql://db725808780.db.1and1.com","dbo725808780","InDon123");
->>>>>>> branch 'NewMaster' of https://github.com/domi-95/InDon.git
 		} catch (Exception e) {
-<<<<<<< HEAD
 			System.out.println("Connection nicht möglich zur DB");
-=======
-			System.out.println("Connection nicht mï¿½glich zum DB");
->>>>>>> branch 'NewMaster' of https://github.com/domi-95/InDon.git
 		}
 	}
 
