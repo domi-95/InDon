4182df3bff2a1dce7e3b6d6f21eae3148c5ac3fa
Kevin <Kevin@Kevin-PC>
2018-02-26 15:37:12 +0100

    pretty printer

--- TestInDon/src/benutzer/Benutzer.java
+++ TestInDon/src/benutzer/Benutzer.java
@@ -1,11 +1,11 @@
 package benutzer;
 
 public class Benutzer {
-	
+
 	private int id;
 	private String benutzername;
 	private String passwort;
-	
+
 	public Benutzer(int id, String benutzername, String passwort) {
 		this.id = id;
 		this.benutzername = benutzername;
@@ -16,15 +16,4 @@ public class Benutzer {
 		return id;
 	}
 
-
-
-
-
-	
-
-	
-	
-	
-	
-	
 }
--- TestInDon/src/benutzer/Mitarbeiter.java
+++ TestInDon/src/benutzer/Mitarbeiter.java
@@ -1,12 +1,9 @@
 package benutzer;
 
+public class Mitarbeiter extends Rettungsorganisation {
 
-public class Mitarbeiter extends Rettungsorganisation{
-	
 	private String name;
 	private String vorname;
-	
-	
 
 	public Mitarbeiter(int id, String benutzername, String passwort, String bezeichnung, int id_ret, String name,
 			String vorname) {
@@ -15,43 +12,21 @@ public class Mitarbeiter extends Rettungsorganisation{
 		this.vorname = vorname;
 	}
 
-
-
-
 	@Override
 	public String toString() {
-		return super.toString()+ "Mitarbeiter [name=" + name + ", vorname=" + vorname + "]";
-	}
-	
-	public int holeId_ret () {
-		return super.getId_ret();
+		return super.toString() + "Mitarbeiter [name=" + name + ", vorname=" + vorname + "]";
 	}
 
+	public int holeId_ret() {
+		return super.getId_ret();
+	}
 
 	public String getName() {
 		return name;
 	}
 
-
 	public String getVorname() {
 		return vorname;
 	}
 
-
-
-	
-
-	
-	
-	
-
-
-	
-	
-
-	
-	
-
-	
-	
 }
