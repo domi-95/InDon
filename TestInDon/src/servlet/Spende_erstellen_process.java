b59b5da7ca305d2bb0f6f279158c49b9922e8e56
Kevin <Kevin@Kevin-PC>
2018-02-28 16:29:04 +0100

    aufräumen von code und unnötigen code

--- TestInDon/src/servlet/Spende_erstellen_process.java
+++ TestInDon/src/servlet/Spende_erstellen_process.java
@@ -3,10 +3,8 @@ package servlet;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -15,228 +13,202 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+import datenbank.ConnectionProvider;
 import spende.Anlaufstelle;
 
 import javax.servlet.http.HttpSession;
 
-
 @WebServlet("/Spende_erstellen_process")
-@MultipartConfig(maxFileSize = 1617721599)	// upload file's size up to 16MB
+@MultipartConfig(maxFileSize = 1617721599) // upload file's size up to 16MB
 public class Spende_erstellen_process extends HttpServlet {
-	//private static final long serialVersionUID = 1L;
-	
-	// database connection settings
-		private String dbURL = "jdbc:mysql://server2.febas.net/InDon";
-		private String dbUser = "InDonAdmin";
-		private String dbPass = "InDon123";
-       
+	private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		Anlaufstelle a = (Anlaufstelle)session.getAttribute("anlauf");
+		Anlaufstelle a = (Anlaufstelle) session.getAttribute("anlauf");
 
 		// gets values of text fields
-				String bezeichnung = request.getParameter("bezeichnung");
-				String beschreibung = request.getParameter("beschreibung");
-				String zustand = request.getParameter("zustand");
-				String vorname = "";
-				String name = "";
-				String adresse = "";
-				String ort = "";
-				String mhd = "";
-				String mail = "";
-				String telefon = "";
-				int abholung = 0;
-				int lieferung = 0;
-				int anonym = 0;
-				int plz = 0;
-				int menge = Integer.parseInt(request.getParameter("menge"));
-				int kat_id= Integer.parseInt(request.getParameter("kategorie"));
-				int bed_id=0;
-				int verfuegbar = 0;
-				int anl_id = a.getId();
-				
-				
-				InputStream inputStream = null;	// input stream of the upload file
-				
-				
-				if("on".equals(request.getParameter("anonym"))) {
-					anonym = 1;
-				}
-				else {
-					anonym = 0;
-				}
+		String bezeichnung = request.getParameter("bezeichnung");
+		String beschreibung = request.getParameter("beschreibung");
+		String zustand = request.getParameter("zustand");
+		String vorname = "";
+		String name = "";
+		String adresse = "";
+		String ort = "";
+		String mhd = "";
+		String mail = "";
+		String telefon = "";
+		int abholung = 0;
+		int lieferung = 0;
+		int anonym = 0;
+		int plz = 0;
+		int menge = Integer.parseInt(request.getParameter("menge"));
+		int kat_id = Integer.parseInt(request.getParameter("kategorie"));
+		int bed_id = 0;
+		int verfuegbar = 0;
+		int anl_id = a.getId();
 
-				vorname = request.getParameter("vorname");
-				name = request.getParameter("name");
-				adresse = request.getParameter("adresse");
-				
-				if("".equals(request.getParameter("plz"))) {
-					plz = 0;
-				}
-				else {
-					plz = Integer.parseInt(request.getParameter("plz"));
-				}
-				
-				ort = request.getParameter("ort"); 
-				mail = request.getParameter("mail");
-				
-				if("".equals(request.getParameter("telefon"))) {
-					telefon = "";
-				}
-				else {
-					telefon = request.getParameter("telefon");
-				}
-				
-				if("2".equals(request.getParameter("lieferungabholung"))){
-					abholung = 1;
-				}
-				if("1".equals(request.getParameter("lieferungabholung"))){
-					lieferung = 1;
-				}
-				
-				
-				// obtains the upload file part in this multipart request
-				Part filePart = request.getPart("photo");
-				if (filePart != null) {
-					// prints out some information for debugging
-					System.out.println(filePart.getName());
-					System.out.println(filePart.getSize());
-					System.out.println(filePart.getContentType());
-					
-					// obtains input stream of the upload file
-					
-					inputStream = filePart.getInputStream();
-			
-				}
-				
-				Connection conn = null;	// connection to the database
-				String message = null;	// message will be sent back to client
-				
-				try {
-					// connects to the database
-					DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-					conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
+		InputStream inputStream = null; // input stream of the upload file
 
-					int count = 0;
-					
-					// constructs SQL statement
-					String sql = "INSERT INTO spende (beschreibung, bezeichnung_spende, zustand, abholung, lieferung, bild, mhd, anonym, vorname, nachname, adresse, plz, ort, anlaufstelle_id, kategorie_id, verfuegbar, beduerftiger_id, menge, email, telefon) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-					PreparedStatement statement = conn.prepareStatement(sql);
-					
-					if("".equals(beschreibung)) {
-						
-					}
-					else {
-						statement.setString(1, beschreibung);
-						count++;
-					}
-					
-					if("".equals(bezeichnung)) {
-						
-					}
-					else {
-						statement.setString(2, bezeichnung);
-						count++;
-					}
-					
-					if("".equals(zustand)) {
-						
-					}else {
-						statement.setString(3, zustand);
-						count++;
-					}
-					
-					statement.setInt(4, abholung);
-					statement.setInt(5, lieferung);
-					
-					if (inputStream != null) {
-						// fetches input stream of the upload file for the blob column
-						statement.setBlob(6, inputStream);
-					}
-					inputStream.close();
-//					if("".equals(mhd)) {
-//						
-//					}
-//					else {
-//						statement.setString(7, mhd);
-//						count++;
-//					}
-					statement.setString(7, mhd);
-					statement.setInt(8, anonym);
-					
-					if("".equals(vorname)) {
-						
-					}
-					else {
-						statement.setString(9, vorname);
-						count++;
-					}
-					
-					if("".equals(name)) {
-						
-					}
-					else {
-						statement.setString(10, name);
-						count++;
-					}
-					
-					if("1".equals(request.getParameter("lieferungabholung"))){
-						count++;
-					}
-					
-					if("2".equals(request.getParameter("lieferungabholung"))){
-						if("".equals(adresse)|| plz==0 || "".equals(ort)) {
-							
-						}
-						else {
-							count++;
-						}
-					}
-					statement.setString(11, adresse);
-					statement.setInt(12, plz);
-					statement.setString(13, ort);
-					if(anl_id != 0) {
-					statement.setInt(14, anl_id);
-					}
-					statement.setInt(15, kat_id);
-					statement.setInt(16, verfuegbar);
-					statement.setInt(17, bed_id);
-					statement.setInt(18, menge);
-					statement.setString(19, mail);
-					statement.setString(20, telefon);
-					
-					
-					
-					// sends the statement to the database server
-					if(count==6) {
-					int row = statement.executeUpdate();
-					if (row > 0) {
-						message = "Vielen Dank, dass sie gespendet haben!";
-					}
-					
-					else {
-						message = "Bitte füllen Sie alle Pflichtfelder!";
-					}
-					}
-				} catch (SQLException ex) {
-					message = "ERROR: " + ex.getMessage();
-					ex.printStackTrace();
-				} finally {
-					if (conn != null) {
-						// closes the database connection
-						try {
-							conn.close();
-						} catch (SQLException ex) {
-							ex.printStackTrace();
-						}
-					}
-					// sets the message in request scope
-					request.setAttribute("Message", message);
-					
-					// forwards to the message page
-					getServletContext().getRequestDispatcher("/Message.jsp").forward(request, response);
+		if ("on".equals(request.getParameter("anonym"))) {
+			anonym = 1;
+		} else {
+			anonym = 0;
+		}
+
+		vorname = request.getParameter("vorname");
+		name = request.getParameter("name");
+		adresse = request.getParameter("adresse");
+
+		if ("".equals(request.getParameter("plz"))) {
+			plz = 0;
+		} else {
+			plz = Integer.parseInt(request.getParameter("plz"));
+		}
+
+		ort = request.getParameter("ort");
+		mail = request.getParameter("mail");
+
+		if ("".equals(request.getParameter("telefon"))) {
+			telefon = "";
+		} else {
+			telefon = request.getParameter("telefon");
+		}
+
+		if ("2".equals(request.getParameter("lieferungabholung"))) {
+			abholung = 1;
+		}
+		if ("1".equals(request.getParameter("lieferungabholung"))) {
+			lieferung = 1;
+		}
+
+		// bezieht das Bild aus dem multipart request
+		Part filePart = request.getPart("photo");
+		if (filePart != null) {
+			// prints out some information for debugging
+			System.out.println(filePart.getName());
+			System.out.println(filePart.getSize());
+			System.out.println(filePart.getContentType());
+
+			// bezieht den input stream vom bild
+
+			inputStream = filePart.getInputStream();
+
+		}
+
+		Connection conn = null; // datenbank connection
+		String message = null;
+
+		try {
+			conn = ConnectionProvider.getCon();
+
+			int count = 0;
+
+			// konstrutiere SQL statement
+			String sql = "INSERT INTO spende (beschreibung, bezeichnung_spende, zustand, abholung, lieferung, bild, mhd, anonym, vorname, nachname, adresse, plz, ort, anlaufstelle_id, kategorie_id, verfuegbar, beduerftiger_id, menge, email, telefon) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+			PreparedStatement statement = conn.prepareStatement(sql);
+
+			if ("".equals(beschreibung)) {
+
+			} else {
+				statement.setString(1, beschreibung);
+				count++;
+			}
+
+			if ("".equals(bezeichnung)) {
+
+			} else {
+				statement.setString(2, bezeichnung);
+				count++;
+			}
+
+			if ("".equals(zustand)) {
+
+			} else {
+				statement.setString(3, zustand);
+				count++;
+			}
+
+			statement.setInt(4, abholung);
+			statement.setInt(5, lieferung);
+
+			if (inputStream != null) {
+				// fetche den input stream vom bild in ein blob column
+				statement.setBlob(6, inputStream);
+			}
+			inputStream.close();
+			statement.setString(7, mhd);
+			statement.setInt(8, anonym);
+
+			if ("".equals(vorname)) {
+
+			} else {
+				statement.setString(9, vorname);
+				count++;
+			}
+
+			if ("".equals(name)) {
+
+			} else {
+				statement.setString(10, name);
+				count++;
+			}
+
+			if ("1".equals(request.getParameter("lieferungabholung"))) {
+				count++;
+			}
+
+			if ("2".equals(request.getParameter("lieferungabholung"))) {
+				if ("".equals(adresse) || plz == 0 || "".equals(ort)) {
+
+				} else {
+					count++;
 				}
 			}
+			statement.setString(11, adresse);
+			statement.setInt(12, plz);
+			statement.setString(13, ort);
+			if (anl_id != 0) {
+				statement.setInt(14, anl_id);
+			}
+			statement.setInt(15, kat_id);
+			statement.setInt(16, verfuegbar);
+			statement.setInt(17, bed_id);
+			statement.setInt(18, menge);
+			statement.setString(19, mail);
+			statement.setString(20, telefon);
+
+			// executet das statement
+			if (count == 6) {
+				int row = statement.executeUpdate();
+				if (row > 0) {
+					message = "Vielen Dank, dass sie gespendet haben!";
+				}
+
+				else {
+					message = "Bitte füllen Sie alle Pflichtfelder!";
+				}
+			}
+		} catch (SQLException ex) {
+			message = "ERROR: " + ex.getMessage();
+			ex.printStackTrace();
+		} finally {
+			if (conn != null) {
+				// schlie�t die db connection
+				try {
+					conn.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+			// sets the message in request scope
+			// setzt die message in den request scope
+			request.setAttribute("Message", message);
+
+			// zur message page
+			getServletContext().getRequestDispatcher("/Message.jsp").forward(request, response);
+		}
+	}
 
 }
